89f0a4bea332f47bfa7d78ba09989205229bd788
youkim <42.4.youkim@gmail.com>
2021-12-15 19:25:00 +0900

    fix: :bug: fix segfault

--- src/ymath/yqsort.c
+++ src/ymath/yqsort.c
@@ -6,7 +6,7 @@
 /*   By: youkim < youkim@student.42seoul.kr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 11:26:06 by youkim            #+#    #+#             */
-/*   Updated: 2021/12/15 16:38:18 by youkim           ###   ########.fr       */
+/*   Updated: 2021/12/15 19:22:18 by youkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,20 @@ static int	partition(int arr[], int left, int right)
 	lo = left + 1;
 	hi = right;
 	pivot = left;
-	while (true)
+	while (lo < hi)
 	{
-		while (arr[lo] < arr[pivot])
+		while (lo < hi && arr[lo] <= arr[pivot])
 			lo++;
 		while (arr[hi] > arr[pivot])
 			hi--;
-		if (lo > hi)
-			break ;
-		yswap(&arr[lo++], &arr[hi--]);
+		if (lo < hi)
+			yswap(&arr[lo], &arr[hi]);
 	}
 	yswap(&arr[pivot], &arr[hi]);
 	return (hi);
 }
 
-static void	quicksort(int *arr, int left, int right)
+static void	quicksort(int arr[], int left, int right)
 {
 	int	pivot;
 
