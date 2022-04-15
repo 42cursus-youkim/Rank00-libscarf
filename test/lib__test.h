4b01134a24b25cf901e0b0f6cef05e61d62e24d5
scarf005 <greenscarf005@gmail.com>
2022-04-15 17:10:24 +0900

    test: :white_check_mark: use macro
    forget it, i'm using macro

---
+++ test/lib__test.h
@@ -0,0 +1,31 @@
+#ifndef LIB__TEST_H
+#define LIB__TEST_H
+
+#include <stdio.h>
+#include "lib__color.h"
+#include "lib__types.h"
+
+void test__header(const t_string text)
+{
+	printf(BHYEL "\n----------------------------------------\n");
+	printf("[[[ Testing " BBLU "%s" BHYEL " ]]]\n", text);
+}
+
+void test__subject(const t_string text)
+{
+	printf(MAG "  [ case " BCYN "%s" MAG " ]\n" END, text);
+}
+
+#define TEST__ASSERT_EQ(left, right)                                         \
+  do {                                                                       \
+    assert(left == right);                                                   \
+    printf(HWHT "{ " BBLU #left " == " #right HWHT " } " BGRN " OK!\n" END); \
+  } while (0);
+
+#define TEST__ASSERT_EXPR(expr)                                \
+  do {                                                         \
+    assert(expr);                                              \
+    printf(HWHT "{ " BBLU #expr HWHT " } " BGRN " OK!\n" END); \
+  } while (0);
+
+#endif
--- test/main.c
+++ test/main.c
@@ -1,32 +1,33 @@
+#include <stdio.h>
+#include <assert.h>
 #include "lib__test.h"
 #include "lib__string.h"
-#include <stdio.h>
 
 void	test__string__cmp(void)
 {
 	test__header("string::cmp");
-	test__assert(str__cmp("abc", "abc", 3) == 0);
-	test__assert(str__cmp("abc", "abcd", 3) == 0);
-	test__assert(str__cmp("abc", "abb", 3) == 1);
-	test__assert(str__cmp("abb", "abc", 3) == -1);
-	test__assert(str__cmp("abcd", "abc", 4) == 'd');
+	TEST__ASSERT_EQ(str__cmp("abc", "abc", 3), 0);
+	TEST__ASSERT_EQ(str__cmp("abc", "abcd", 3), 0);
+	TEST__ASSERT_EQ(str__cmp("abc", "abb", 3), 1);
+	TEST__ASSERT_EQ(str__cmp("abb", "abc", 3), -1);
+	TEST__ASSERT_EQ(str__cmp("abcd", "abc", 4), 'd');
 }
 
 void	test__string__len(void)
 {
 	test__header("str::len");
-	test__assert(str__len("abc") == 3);
-	test__assert(str__len("") == 0);
-	test__assert(str__len(NULL) == -1);
+	TEST__ASSERT_EQ(str__len("abc"), 3);
+	TEST__ASSERT_EQ(str__len(""), 0);
+	TEST__ASSERT_EQ(str__len(NULL), -1);
 }
 
 void	test__string__is_equal(void)
 {
 	test__header("str::is_equal");
-	test__assert(str__is_equal("hello world", "hello world"));
-	test__assert(!str__is_equal("hello world", "hello world!"));
-	test__assert(!str__is_equal("hello world", "bye world"));
-	test__assert(!str__is_equal("hello world", NULL));
+	TEST__ASSERT_EXPR(str__is_equal("hello world", "hello world"));
+	TEST__ASSERT_EXPR(not str__is_equal("hello world", "hello world!"));
+	TEST__ASSERT_EXPR(not str__is_equal("hello world", "bye world"));
+	TEST__ASSERT_EXPR(not str__is_equal("hello world", NULL));
 }
 
 int	main(void)
