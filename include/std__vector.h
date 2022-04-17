7efa9c9a308f629d6afd8ff2de4badc78480b150
scarf <greenscarf005@gmail.com>
2022-04-17 17:08:21 +0900

    feat: :sparkles: vector rotation

--- include/std__vector.h
+++ include/std__vector.h
@@ -1,15 +1,22 @@
 #ifndef STD__VECTOR_H
 # define STD__VECTOR_H
 
-typedef struct s_vec
-{
-	float	x;
-	float	y;
-}	t_vec;
+typedef struct s_vec {
+  double x;
+  double y;
+} t_vec;
+
+typedef struct s_ivec {
+  int x;
+  int y;
+} t_ivec;
 
 //@func
 /*
 ** < math.c > */
 
-t_vec	vec__add(t_vec *v1, t_vec *v2);
+t_vec	vec__add(t_vec* v1, t_vec* v2);
+t_vec	vec__addx(t_vec* v1, double x);
+t_vec	vec__addy(t_vec* v1, double y);
+t_vec	vec__rotate(t_vec* vec, double angle);
 #endif
--- src/std__vector/math.c
+++ src/std__vector/math.c
@@ -1,13 +1,34 @@
 #include "std__vector.h"
+#include <math.h>
 
-t_vec	vec__add(t_vec *v1, t_vec *v2)
+t_vec	vec__add(t_vec* v1, t_vec* v2)
 {
-	const t_vec	res = {
-		v1->x + v2->x,
-		v1->y + v2->y,
-	};
+  return (t_vec){
+      v1->x + v2->x,
+      v1->y + v2->y,
+  };
+}
 
-	// res.x = v1->x + v2->x;
-	// res.y = v1->y + v2->y;
-	return (res);
+t_vec	vec__addx(t_vec* v1, double x)
+{
+	return (t_vec){
+			v1->x + x,
+			v1->y,
+	};
+}
+
+t_vec vec__addy(t_vec* v1, double y)
+{
+	return (t_vec){
+			v1->x,
+			v1->y + y,
+	};
+}
+
+t_vec	vec__rotate(t_vec* vec, double angle)
+{
+	return (t_vec){
+		.x = vec->x * cos(angle) - vec->y * sin(angle),
+		.y = vec->x * sin(angle) + vec->y * cos(angle),
+	};
 }
